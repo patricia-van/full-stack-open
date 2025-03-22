4e904166b753919c9c7cf335dd2a558ba7b6d2fc
pvsantoso <psantos@ampol.com.sg>
2025-03-23 01:04:37 +0800

    add token and user extractor middleware

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,3 +1,26 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+
+  next()
+}
+
+const userExtractor =  async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  request.user = await User.findById(decodedToken.id)
+
+  next()
+}
+
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -9,12 +32,16 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return response.status(400).json({ error: 'expected `username` to be unique' })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
   next(error)
 }
 
 module.exports = {
+  tokenExtractor,
+  userExtractor,
   unknownEndpoint,
   errorHandler
 }
