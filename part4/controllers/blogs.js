a081abbfb3f327e42fff09786b0f0c5948b2e1ed
pvsantoso <psantos@ampol.com.sg>
2025-03-23 00:19:49 +0800

    add users to blog app

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const { title, author, url, likes } = request.body
+  const { title, author, url, likes, userId } = request.body
+
+  const user = await User.findById(userId)
 
   if (!title || !url) {
     return response.status(400).json({ error: 'title or url missing'})
@@ -17,10 +21,14 @@ blogsRouter.post('/', async (request, response) => {
     title: title,
     author: author,
     url: url,
+    user: user.id,
     likes: likes ?? 0
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
