49a332ffc6c2f96e5916a565594ae7600d7ee0b5
pvsantoso <psantos@ampol.com.sg>
2025-03-22 23:49:02 +0800

    add users to blogs

--- part4/app.js
+++ part4/app.js
@@ -1,16 +1,23 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app 
