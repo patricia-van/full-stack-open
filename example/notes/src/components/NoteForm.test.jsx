0deb7da0cc6f7d27c14ee98517917feccbb45da7
pvsantoso <psantos@ampol.com.sg>
2025-03-23 22:12:54 +0800

    add test for notes frontend

---
+++ example/notes/src/components/NoteForm.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import NoteForm from './NoteForm'
+import userEvent from '@testing-library/user-event'
+
+test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+  const createNote = vi.fn()
+  const user = userEvent.setup()
+
+  render(<NoteForm createNote={createNote} />)
+
+//   const input = screen.getByRole('textbox')
+  const input = screen.getByPlaceholderText('write note content here')
+//   const input = container.querySelector('#note-input')
+  const sendButton = screen.getByText('save')
+
+  await user.type(input, 'testing a form...')
+  await user.click(sendButton)
+
+  console.log(createNote.mock.calls)
+
+  expect(createNote.mock.calls).toHaveLength(1)
+  expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
+})
