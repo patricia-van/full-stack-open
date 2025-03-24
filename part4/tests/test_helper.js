75a0ea317a45eb1b949919cceb547c7025808a78
pvsantoso <psantos@ampol.com.sg>
2025-03-24 13:25:01 +0800

    fix blog api tests

--- part4/tests/test_helper.js
+++ part4/tests/test_helper.js
@@ -1,25 +1,58 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    title: 'HTML is easy',
-    author: 'blogger1',
-    url: 'http://',
-    likes: 100
+    title: "First Blog",
+    author: "Author 1",
+    url: "http://example.com/1",
+    likes: 5
   },
   {
-    title: 'Browser can execute only JavaScript',
-    author: 'javascript',
-    url: 'http://js.com',
+    title: "Second Blog",
+    author: "Author 2",
+    url: "http://example.com/2",
     likes: 10
+  },
+  {
+    title: "Third Blog",
+    author: "Author 3",
+    url: "http://example.com/3",
+    likes: 7
   }
 ]
 
-const blogsInDb = async () => {
+const initialUsers = [
+  {
+    username: 'root',
+    name: 'root',
+    password: 'root'
+  },
+  {
+    username: 'fail',
+    name: 'fail',
+    password: 'fail'
+  }
+]
+
+const loginUser = {
+    username: 'login',
+    password: 'secret'
+  }
+
+blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
 }
 
+usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  initialBlogs, blogsInDb
+  initialBlogs,
+  blogsInDb,
+  usersInDb
 }
