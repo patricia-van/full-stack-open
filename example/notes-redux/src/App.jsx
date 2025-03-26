695ccc16bbac98e99f8d367819566ae07e2f8ca8
pvsantoso <psantos@ampol.com.sg>
2025-03-26 12:08:41 +0800

    add filter

--- example/notes-redux/src/App.jsx
+++ example/notes-redux/src/App.jsx
@@ -1,10 +1,12 @@
 import Notes from './components/Notes'
 import NewNote from './components/NewNote'
+import VisibilityFilter from './components/visibilityFilter'
 
 const App = () => {
   return(
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
