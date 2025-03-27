a8d05d15f91f437a0cbc27891fe06e269595b7d8
pvsantoso <psantos@ampol.com.sg>
2025-03-28 00:15:30 +0800

    use react query

--- example/notes-redux/src/main.jsx
+++ example/notes-redux/src/main.jsx
@@ -1,12 +1,30 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { Provider } from 'react-redux'
+// import store from './store'
+// import App from './App'
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import App from './App'
 
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>
+
+  </QueryClientProvider>
 )
